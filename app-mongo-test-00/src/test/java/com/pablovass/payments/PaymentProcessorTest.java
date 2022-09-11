6d38d2a4e0bb08920a0c0a59d193d0719713632b
pablo vallejos <pablo.vallejos@naturalth.it>
2022-09-11 20:06:59 -0300

    pasarela de pago

---
+++ app-mongo-test-00/src/test/java/com/pablovass/payments/PaymentProcessorTest.java
@@ -0,0 +1,31 @@
+package com.pablovass.payments;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentProcessorTest {
+    @Test
+    void payment_is_correct() {
+        //como no podemos hacerder al payment gateway lo sumulamos con moquito
+        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
+        Mockito.when(paymentGateway.resquestPayment(Mockito.any()))
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
+        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
+      // boolean result =paymentProcessor.makePayment(1000);
+       //assertTrue(result);
+       assertTrue(paymentProcessor.makePayment(1000));
+
+    }
+    @Test
+    void payment_is_wrong() {
+        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
+        Mockito.when(paymentGateway.resquestPayment(Mockito.any()))
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
+        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
+        assertFalse(paymentProcessor.makePayment(1000));
+
+    }
+}
