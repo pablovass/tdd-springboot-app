431e23ba42fee25dd3885cfd49f9eece60c0080d
pablo vallejos <pablo.vallejos@naturalth.it>
2022-09-11 20:25:16 -0300

    obtimizando el test

--- app-mongo-test-00/src/test/java/com/pablovass/payments/PaymentProcessorTest.java
+++ app-mongo-test-00/src/test/java/com/pablovass/payments/PaymentProcessorTest.java
@@ -1,29 +1,36 @@
 package com.pablovass.payments;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentProcessorTest {
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    // se guardan las partes comunes
+    @BeforeEach // es el @Before junit 4
+    public void setup() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     void payment_is_correct() {
-        //como no podemos hacerder al payment gateway lo sumulamos con moquito
-        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.resquestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
-      // boolean result =paymentProcessor.makePayment(1000);
-       //assertTrue(result);
-       assertTrue(paymentProcessor.makePayment(1000));
+        assertTrue(paymentProcessor.makePayment(1000));
 
     }
+
     @Test
     void payment_is_wrong() {
-        PaymentGateway paymentGateway= Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.resquestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
 
